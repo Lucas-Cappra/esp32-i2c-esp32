636549799e2d0a336233ed570d3f2eb79330d3e7
Lucas-Cappra <108031562+Lucas-Cappra@users.noreply.github.com>
2025-11-27 10:08:41 -0300

    Update slave.ino

--- slave.ino
+++ slave.ino
@@ -1,18 +1,24 @@
-#include "Wire.h"
+#include <Wire.h>
+#include "SensorDHT.h"
 
-#define I2C_DEV_ADDR 0x55
+#define DHTPIN 4
+#define DHTTYPE DHT22
 
-uint32_t i = 0;
+#define I2C_DEV_ADDR 0x55   // endereço do ESP32 como SLAVE
 
+SensorDHT sensorDHT(DHTPIN, DHTTYPE);
+
+// Buffer que enviaremos ao Master
+char i2cBuffer[32];
+
+// Quando o Master pedir dados (I2C request)
 void onRequest() {
-  Wire.print(i++);
-  Wire.print(" Packets.");
-  Serial.println("onRequest");
-  Serial.println();
+  Wire.slaveWrite((uint8_t*)i2cBuffer, strlen(i2cBuffer));
 }
 
+// Quando o Master enviar algo para o Slave (opcional)
 void onReceive(int len) {
-  Serial.printf("onReceive[%d]: ", len);
+  Serial.printf("Master enviou %d bytes: ", len);
   while (Wire.available()) {
     Serial.write(Wire.read());
   }
@@ -21,18 +27,31 @@ void onReceive(int len) {
 
 void setup() {
   Serial.begin(115200);
-  Serial.setDebugOutput(true);
+  sensorDHT.begin();
+
+  // Inicializa como I2C Slave
   Wire.onReceive(onReceive);
   Wire.onRequest(onRequest);
   Wire.begin((uint8_t)I2C_DEV_ADDR);
 
-/*#if CONFIG_IDF_TARGET_ESP32
-  char message[64];
-  snprintf(message, 64, "%lu Packets.", i++);
-  Wire.slaveWrite((uint8_t *)message, strlen(message));
-  Serial.print('Printing config %lu', i);
-#endif*/
+  Serial.println("ESP32 pronto como I2C Slave!");
 }
 
 void loop() {
+  float temperature = sensorDHT.readTemperature();
+  float humidity = sensorDHT.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Erro ao ler DHT22");
+    delay(1000);
+    return;
+  }
+
+  // Formata a resposta que será enviada ao Master
+  snprintf(i2cBuffer, sizeof(i2cBuffer), "%.2f;%.2f", temperature, humidity);
+
+  Serial.print("Atualizado buffer I2C -> ");
+  Serial.println(i2cBuffer);
+
+  delay(2000); // Atualiza a cada 2s
 }
